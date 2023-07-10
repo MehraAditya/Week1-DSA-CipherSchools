3a59d9f3bf5c34b7f21edbc798c28567043c241f
Aditya Mehra <95765340+MehraAditya@users.noreply.github.com>
2023-07-10 12:38:18 +0530

    Add files via upload
    primitive data types

---
+++ PrimitiveDT.java
@@ -0,0 +1,20 @@
+public class PrimitiveDT {
+    public static void main(String[] args) {
+        int x=10;
+        int y = 20;
+        System.out.println("x is : "+x);
+        System.out.println("y is : "+y);
+
+        byte b=29;
+        byte b1 =(byte)128;
+        System.out.println("b is : "+b);
+        System.out.println("b is : "+b1);
+
+        int z = b;
+        System.out.println(" z is : "+z);
+
+        byte b2 = (byte)y;
+        System.out.println("b2 is : "+b2);
+    }
+    
+}
